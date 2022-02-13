a61292e61c206744c69e04798ef1f17c6dbe6219
sagittariusasparagus <sagittariusasparagus@gmail.com>
2022-02-13 15:51:14 -0600

    added prelim rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+function computerPlay () {
+    let computerChoices = ["rock", "paper", "scissors"];
+    return computerChoices [Math.floor(Math.random() * computerChoices.length)];
+}
+
+function gamePlay (playerSelection, computerSelection) {
+if (playerSelection == "rock" && computerSelection == "scissors")
+{return "You won!"}
+else if (playerSelection == "rock" && computerSelection == "paper")
+{ return "You lost!"}
+else if (playerSelection == "paper" && computerSelection == "scissors")
+{ return "You lost!"}
+else if (playerSelection == "paper" && computerSelection == "rock")
+{ return "You won!"}
+else if (playerSelection == "scissors" && computerSelection == "paper")
+{ return "You won!"}
+else if (playerSelection == "scissors" && computerSelection == "rock")
+{ return "You lost!"}
+else {return "It's a tie!!!"}
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay()
+
+console.log(gamePlay(playerSelection,computerSelection))
