f0b4b3ce9b14d88d3d95661aff1ed2f404d9476f
sagittariusasparagus <sagittariusasparagus@gmail.com>
2022-02-14 07:46:27 -0600

    added ability to play game 5 x

--- javascript.js
+++ javascript.js
@@ -1,25 +1,50 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay () {
     let computerChoices = ["rock", "paper", "scissors"];
     return computerChoices [Math.floor(Math.random() * computerChoices.length)];
 }
 
-function gamePlay (playerSelection, computerSelection) {
-if (playerSelection == "rock" && computerSelection == "scissors")
-{return "You won!"}
-else if (playerSelection == "rock" && computerSelection == "paper")
-{ return "You lost!"}
-else if (playerSelection == "paper" && computerSelection == "scissors")
-{ return "You lost!"}
-else if (playerSelection == "paper" && computerSelection == "rock")
-{ return "You won!"}
-else if (playerSelection == "scissors" && computerSelection == "paper")
-{ return "You won!"}
-else if (playerSelection == "scissors" && computerSelection == "rock")
-{ return "You lost!"}
-else {return "It's a tie!!!"}
+function gamePlay () {
+    const playerInput = prompt("Rock, paper, or scissors?", );
+    const playerSelection = playerInput.toLowerCase();
+    const computerSelection = computerPlay().toLowerCase();
+    
+    if (playerSelection == "rock" && computerSelection == "scissors")
+        {return "You won!";
+        playerScore += 1}
+    else if (playerSelection == "rock" && computerSelection == "paper")
+        { return "You lost!";
+        computerScore += 1}
+    else if (playerSelection == "paper" && computerSelection == "scissors")
+        { return "You lost!";
+        computerScore += 1}
+    else if (playerSelection == "paper" && computerSelection == "rock")
+        { return "You won!";
+        playerScore += 1}
+    else if (playerSelection == "scissors" && computerSelection == "paper")
+        { return "You won!";
+        playerScore += 1}
+    else if (playerSelection == "scissors" && computerSelection == "rock")
+        { return "You lost!";
+        computerScore += 1}
+    else {return "It's a tie!!!"}
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay()
+function game() {
+    
+        for (let i = 0; i < 5; i++){
+            console.log(gamePlay());
+        }
+    }
 
-console.log(gamePlay(playerSelection,computerSelection))
+
+
+
+
+game();
+console.log(playerScore);
+console.log(computerScore)
+
